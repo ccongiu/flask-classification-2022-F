49270899acf67004ef4a412ef9ddd71d6bb0447d
ccongiu <58791057+ccongiu@users.noreply.github.com>
2024-03-02 12:19:14 +0100

    Modified init uploaded

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,3 +2,5 @@ from .classifications import *
 from .classifications_id import *
 from .info import *
 from .home import *
+
+from .classification_on_uploaded import * # Added for the classification from an uploaded image
